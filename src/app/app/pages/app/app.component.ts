af2d4bd06268f7028082eac9822f7b69c31f06e0
Sebastian Golian <sebastiangolian@gmail.com>
2020-11-11 19:09:52 +0100

    app - header hide

--- src/app/app/pages/app/app.component.ts
+++ src/app/app/pages/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { environment } from 'src/environments/environment';
@@ -12,13 +13,25 @@ export class AppComponent {
   isCollapsed = true;
   title = 'angular-shop-mk';
   environments: any = environment
+  init: boolean = false
 
   private _subscription: Subscription = new Subscription();
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private route: ActivatedRoute, private router: Router) {
+    this._subscription.add(this.displayFooter())
+  }
 
   onLogOut() {
     this._subscription.add(this.authService.logoutSubscription())
+  }
+
+  private displayFooter(): Subscription {
+    return this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.init = false
+        setTimeout(() => this.init = true, 200);
+      }
+    });
   }
 
   ngOnDestroy() {
