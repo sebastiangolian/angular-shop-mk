7f37587d69ccf270807979bbfded37f0e46ded8d
sebastiangolian <sebastiangolian@gmail.com>
2021-01-18 11:40:22 +0100

    photo-modal - setPosition

--- src/app/photo/components/photo-modal-image/photo-modal-image.component.ts
+++ src/app/photo/components/photo-modal-image/photo-modal-image.component.ts
@@ -16,34 +16,61 @@ export class PhotoModalImageComponent implements OnChanges {
   @Output() prev: EventEmitter<boolean> = new EventEmitter();
   @Output() next: EventEmitter<boolean> = new EventEmitter();
 
-  leftRight: number = 22
+  right: number = 22
+  left: number = 22
+  top: number = 260
   constructor() { }
 
   @HostListener('window:resize', ['$event'])
   ngOnChanges() {
-    this.getLeftRight()
+    this.setPosition()
   }
 
-  getLeftRight() {
-    if (this.photo.height >= 600) {
+  setPosition() {
+    if (this.isVertical()) {
+      this.top = 260
       switch (true) {
         case window.innerWidth < 445:
-          this.leftRight = 22
+          this.top = this.top + (window.innerWidth - 445) / 2
+          this.left = this.right = 22
           break;
         case window.innerWidth >= 445 && window.innerWidth < 576:
-          this.leftRight = 22 + (window.innerWidth - 445) / 2
+          this.left = this.right = 22 + (window.innerWidth - 445) / 2
           break;
         case window.innerWidth >= 576 && window.innerWidth < 992:
-          this.leftRight = 55
+          this.left = this.right = 55
           break;
         case window.innerWidth >= 992 && window.innerWidth < 1200:
-          this.leftRight = 22
+          this.left = this.right = 22
           break;
         default:
-          this.leftRight = 90
+          this.top = 270
+          this.left = this.right = 90
       }
     } else {
-      this.leftRight = 22
+      this.left = this.right = 22
+
+      switch (true) {
+        case window.innerWidth < 445:
+          this.top = 130 + (window.innerWidth - 445) / 2
+          break;
+        case window.innerWidth < 992:
+          this.top = 130
+          break;
+        case window.innerWidth < 1200:
+          this.top = 100
+          break;
+        default:
+          this.top = 150
+      }
+    }
+  }
+
+  isVertical(): boolean {
+    if (this.photo.height > this.photo.width) {
+      return true
+    } else {
+      return false
     }
   }
 
